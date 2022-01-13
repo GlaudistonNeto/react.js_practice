ac37d811cab10bbc449c6e6edf25bbb92e3b8f97
GlaudistonNeto <glaudistonson@gmail.com>
2022-01-12 23:11:49 -0300

    practicing components

--- src/App.js
+++ src/App.js
@@ -1,46 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
+class App extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {
-      name: 'G-Neto',
-      counter: 0,
-    };
-
-    this.increase = this.increase.bind(this);
-    this.decrease = this.decrease.bind(this);
-  };
-
-  increase() {
-    let state = this.state;
-    state.counter++;
-    state.name = 'jaunzinho';
-    this.setState(state);
-  }
-
-  decrease() {
-    let state = this.state;
-    if (state.counter === 0) {
-      alert("You can't keep decreasing anymore, sorry...")
-      return;
-    } else {
-      state.counter--;
-      state.name = 'zezim';
-      this.setState(state);
+      hour: '00:00:00'
     }
   }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ hour: new Date().toLocaleTimeString() })
+    }, 1000);
+  }
+
+  // componentDidUpdate() {
+  //   alert('Updated!');
+  // };
+
+  // shouldComponentUpdate() {
+  //   this.state.hour = true;
+  // };
 
   render() {
     return (
       <div>
-      <h1>This is a number from the counter</h1>
-        {this.state.name}
-        <h3>
-          <button onClick={this.decrease}>-</button>
-            {this.state.counter}
-          <button onClick={this.increase}>+</button>
-        </h3>
+        <p><h1>My Project {this.state.hour}</h1></p>
       </div>
     );
   }
