0c48db767b5dfa88a7a005aef4a84760727edfb7
GlaudistonNeto <glaudistonson@gmail.com>
2022-01-13 00:49:50 -0300

    practicing conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,33 +1,59 @@
 import React from 'react';
-import Member from './components/Member';
+// import Member from './components/Member';
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      hour: '00:00:00'
+      status: false
     }
+
+    this.out = this.out.bind(this);
+    this.enter = this.enter.bind(this);
   }
 
-  componentDidMount() {
-    setInterval(() => {
-      this.setState({ hour: new Date().toLocaleTimeString() })
-    }, 1000);
+  out() {
+    this.setState({ status: false });
   }
 
-  // componentDidUpdate() {
-  //   alert('Updated!');
-  // };
+  enter() {
+    this.setState({ status: true });
+  }
 
-  // shouldComponentUpdate() {
-  //   this.state.hour = true;
-  // };
+  // enter() {
+  //   this.setState({ status: true });
+  // }
+
+  // componentDidMount() {
+  //   setInterval(() => {
+  //     this.setState({ hour: new Date().toLocaleTimeString() })
+  //   }, 1000);
+  // }
+
+  // // componentDidUpdate() {
+  // //   alert('Updated!');
+  // // };
+
+  // // shouldComponentUpdate() {
+  // //   this.state.hour = true;
+  // // };
 
   render() {
     return (
       <div>
-        <Member name="Owner" />
+        {
+          this.state.status ?
+          <div>
+            <h1>You took the Red pill WELCOME TO THE MATRIX</h1>
+            <button onClick={this.out}>Take the Blue pill</button>
+          </div>
+        :
+        <div>
+          <h2>You took the Blue pill GO BACK TO YOUR OLD LIFE!</h2>
+          <button onClick={this.enter}>Take the Red pill</button>
+        </div>
+        }
       </div>
     );
   }
