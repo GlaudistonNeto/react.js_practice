fbab51b39c312f4ccd65fb70d2078efc3554dd6d
GlaudistonNeto <glaudistonson@gmail.com>
2022-01-14 20:41:52 -0300

    prcticing forms

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Feed from './components/Feed';
+// import Feed from './components/Feed';
 // import Member from './components/Member';
 
 class App extends React.Component {
@@ -7,15 +7,16 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      feed: [
-        {id: 1, username: 'G-neto', likes: 10, comments: 15},
-        {id: 2, username: 'Putu', likes: 100, comments: 155},
-        {id: 3, username: 'Kabongo', likes: 12, comments: 18},
-      ]
+      email: '',
+      password: '',
+      gender: 'male',
     }
 
     // this.out = this.out.bind(this);
     // this.enter = this.enter.bind(this);
+
+    this.CMail = this.CMail.bind(this);
+    this.CGender = this.CGender.bind(this);
   }
 
   // out() {
@@ -43,20 +44,45 @@ class App extends React.Component {
   // // shouldComponentUpdate() {
   // //   this.state.hour = true;
   // // };
+  CMail = (e) => {
+    let typedValue = e.target.value;
+    this.setState({email: typedValue});
+  }
 
+  CGender(e) {
+    this.setState({gender: e.target.value});
+  }
+ 
   render() {
     return (
       <div>
-        {this.state.feed.map((item) => {
-          return (
-            <Feed
-              id={item.id}
-              username={item.username}
-              likes={item.likes}
-              comments={item.comments}
-            />
-          );
-        })}
+        <p>Email:
+        <input
+          type="text"
+          value={this.state.email}
+          onChange={this.CMail}
+          placeholder='email@email.com'
+          name='email' /><br />
+        Password:
+        <input
+          type="password"
+          value={this.state.password}
+          onChange={(e) => {this.setState({password: e.target.value})}}
+          placeholder='********'
+          name='password' /></p>
+          <select
+            name="gender"
+            value={this.state.gender}
+            onChange={this.CGender}
+          >
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+          <div>
+            <h3>{this.state.email}</h3>
+            <h3>{this.state.password}</h3>
+            <h3>{this.state.gender}</h3>
+          </div>
       </div>
     );
   }
